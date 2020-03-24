df2d44a78fa8bd8b66f751b668213ac506c9212d
Joshua <jbautista396@gmail.com>
2020-03-23 20:03:16 -0400

    Updates

---
+++ precache-manifest.495968443072e1d3e680e8fb2af3a397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af32a76d0932fa64567bba389b0bf3be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46d16314e68c2ecf55be",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16f73180b78bfbeacd11",
+    "url": "/static/js/2.7ff186fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ff186fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46d16314e68c2ecf55be",
+    "url": "/static/js/main.54e73ff7.chunk.js"
+  },
+  {
+    "revision": "734a0405fc21c06db118",
+    "url": "/static/js/runtime-main.dbc79028.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
