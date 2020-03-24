3187716e39576796f39d540a02c86190d5ed0cd4
Joshua <jbautista396@gmail.com>
2020-03-23 21:43:55 -0400

    Initialize setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from "./sections/Home";
+import About from "./sections/About";
+import Projects from "./sections/Projects";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Under Construction</h1>
+      <Home />
+      <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/sections/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from '@material-ui/core/Container';
+
+ 
+function Home(props) {
+  return (
+    <Container>
+
+      <h1>Hello, I'm Joshua Bautista</h1>
+
+    </Container>
+  )
+}
+ 
+export default Home;
