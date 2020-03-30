3a3ee523af91743b3830beebf8ac400fb0a60402
Joshua <jbautista396@gmail.com>
2020-03-30 19:11:47 -0400

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Hello, I'm Joshua Bautista/i);
   expect(linkElement).toBeInTheDocument();
 });
