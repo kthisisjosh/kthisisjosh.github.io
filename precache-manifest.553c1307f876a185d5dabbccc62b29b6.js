4c14ecd7059ceffcfd64257cbf3c84681a4162e2
Joshua <jbautista396@gmail.com>
2020-03-31 13:06:59 -0400

    Updates

---
+++ precache-manifest.553c1307f876a185d5dabbccc62b29b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb3eaf0cf024119558ae",
+    "url": "/static/css/main.412fa04a.chunk.css"
+  },
+  {
+    "revision": "cb3eaf0cf024119558ae",
+    "url": "/static/js/main.cb3eaf0c.chunk.js"
+  },
+  {
+    "revision": "a0a9ad00a9ba81737f1d",
+    "url": "/static/js/1.a0a9ad00.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a54fd84a6ad772408422cdf8fec5aa2d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1be3244f5658533e39cd2e5c04dac827.js"
+  "/precache-manifest.553c1307f876a185d5dabbccc62b29b6.js"
 );
 
 workbox.clientsClaim();
